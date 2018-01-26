37622442056a36dea4dc8014bdc62aa0d9aced89
gselli12 <tgsellmann@gmail.com>
2018-01-26 23:04:51 +0100

    recipe 5

--- recipe4.py
+++ recipe4.py
@@ -9,7 +9,6 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.5)
 
 # from sklearn import tree
 # my_classifier = tree.DecisionTreeClassifier()
-
 from sklearn.neighbors import KNeighborsClassifier
 my_classifier = KNeighborsClassifier()
 
